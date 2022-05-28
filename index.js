2180f01437cca691678ff6d482006cfa141d4afe
hoanganhfmtd <hoanganhfmtd@gmail.com>
2022-05-28 14:26:23 +0700

    add init project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(8080, (err) => {
+    
+    if(err) throw err
+    console.log('Sever started')
+})
